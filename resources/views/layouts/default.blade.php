f90263a3de20ae654699f99de2d8be54b9b83f15
unknown <>
2017-03-07 16:49:51 +0800

    Changed footer default and header

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
-    <title>@yield('title', 'Sample')</title>
+    <title>@yield('title', 'LLHH')</title>
 </head>
 <body>
     @include(('layouts._header'))
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>Hello Laravel</h1>
+        <h1>Hello LLHH</h1>
         <p class="lead">
-            你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a>的示例项目主页。
+            你现在所看到的是LLHH习题库管理系统的示例项目主页。
         </p>
         <p>
             一切，将从这里开始。
