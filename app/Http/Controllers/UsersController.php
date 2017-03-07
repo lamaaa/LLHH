12001938070811f946db34e9da5dadf665884bce
unknown <>
2017-03-07 14:57:16 +0800

    Initital commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'only'  =>  ['edit', 'update', 'destroy']
+        ]);
+
+        $this->middleware('guest', [
+            'only'  =>  ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(30);
+        return view('users.index', compact('users'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'  =>  'required|max:50',
+            'email' =>  'required|email|unique:users|max:255',
+            'password'  =>  'required'
+        ]);
+
+        $user = User::create([
+            'name'  =>  $request->name,
+            'email' =>  $request->email,
+            'password'  =>  bcrypt($request->password),
+        ]);
+
+        //Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~,请先前往邮箱进行激活。');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'  =>  'required|max:50',
+            'password'  =>  'confirmed|min:6'
+        ]);
+
+        $user = User::findOrFail($id);
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password)
+        {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $id);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'yang-19950427@163.com';
+        $name = 'Lam';
+        $to = $user->email;
+        $subject = '感谢注册Sample应用！请确认您的邮箱。';
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject){
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜您，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+Route::resource('users', 'UsersController');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+Route::get('password/email', 'Auth\PasswordController@getEmail')->name('password.reset');
+Route::post('password/email', 'Auth\PasswordController@postEmail')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+Route::post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');
