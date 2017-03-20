a81fa8ed79c51d9e4266d72967a428b6f07afc50
Aranl <893916371@qq.com>
2017-03-21 01:14:51 +0800

    修改了图片名称，增加图片too.jpg,增加了文件fons

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -3,5 +3,82 @@
 @section('title', '关于')
 
 @section('content')
-    <h1>关于页</h1>
+     <div class="container makerting">
+      <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 ">
+          <h2 >|关于我们 <!-- class="featurette-heading" <span class="text-muted">It'll blow your mind.</span>--></h2>
+          <p >
+          LLHH现为高考数学题库，该网站于2016年2月28日创建。</br>
+          LLHH网站可以让学生更容易在线写题，即时获得答案。可以通过知识点，难度，错题频率，类型来学习高考数学。同时也可以在线做题，并即时获得答案。通过用户做题后提交自身的难易度判断让其它用户得到最切身的反馈。本网站还有各地区各年份的真题卷，同学们能模拟答题对自身成绩有一个判定。同时，登录后还可以查看之前做过的题目与自己错误的题目及其错误次数，也使同学们更近一步的对自身知识点的掌握情况有了一定的了解。
+           </p>
+         
+          <p>
+          LLHH最大的特色在于可以自动出题，用户可以根据自身情况自定义卷子。</p>
+
+          <p>网站目前开设有题目、试卷、易错题三个板块。</p>
+          
+          <p>题目区：所有的题目，根据各种分类。</br>
+             试卷区：以最快、最新、最全的国内各个地区各个年份的高考数学真题卷。<br>
+             易错区：根据所有用户的做题情况整理。
+          </p>
+        </div>
+
+        <div class="col-md-5 col-md-push-2">
+          <h4>&nbsp</h4>
+          <h3>|联系合作</h3>
+          <a href="linhaoyangchishi@gmail.com">linhaoyangchishi@gmail.com</a>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+
+      <div class="row featurette">
+        <div class="col-md-7 ">
+
+          <h2 > |小事记 <!-- class="featurette-heading" <span class="text-muted"></span> --></h2>
+          <p>2017年2月28日，LLHH的ideal正式提出啦。</p>
+          <p>2017年2月29日，LLHH分工完成开始讨论啦。</p>
+          <p>2017年3月1日，LLHH的需求分析出炉啦。</p>
+          <p>2017年3月2日，本网站正式命名为LLHH啦。</p>
+          <p>2017年3月2日，本网站正式命名为LLHH啦。</p>
+          <p>2017年3月4日，设计君被打回去重做主页啦。</p>
+          <p>2017年3月7日，新的主页又做好啦。</p>
+          <p>2017年3月8日，设计君终于被逼疯了。</p>
+          <p>2017年3月10日，前端SAMA被组长‘鼓励’做完网页啦。</p>
+          <p>2017年3月11日，剩余的网站开始设计啦。</p>
+          <p>2017年3月12日，设计君卒。</p>
+        </div>
+        
+        <div class="col-md-5 col-md-push-2 ">
+
+          <h3>|小组成员</h3>
+          <p class="lead" >
+              <em>&nbsp&nbsp</em>林浩阳 &nbsp 25<br>
+              <em>&nbsp&nbsp</em>林键燃 &nbsp 26<br>
+              <em>&nbsp&nbsp</em>何瑞博 &nbsp 04<br>
+              <em>&nbsp&nbsp</em>何玥 &nbsp&nbsp&nbsp&nbsp&nbsp 54 </p>
+          <p class="lead"> </p>
+          <p class="lead"> </p>
+          <p class="lead"></p>
+        </div>
+      </div>
+
+
+
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">回到顶部</a></p>
+        <p class="text-center">&copy; Copyright 2017 -LLHH项目小组- 林浩阳 林键燃 何瑞博 何玥  </br><p class="text-center"><a href="#" >粤ICP14151106号</a></p>  </p>
+      </footer>
+
+    </div><!-- /.container -->
 @stop
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -12,7 +12,7 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img class="first-slide" src="/img/default.jpg" alt="First slide">
+                <img class="first-slide" src="/img/carousel_page1.jpg" alt="First slide">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>难题</h1>
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div class="item">
-                <img class="second-slide" src="/img/default1.jpg" alt="Second slide">
+                <img class="second-slide" src="/img/carousel_page2.jpg" alt="Second slide">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>模拟题</h1>
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="item">
-                <img class="third-slide" src="img/default2.jpg" alt="Third slide">
+                <img class="third-slide" src="img/carousel_page3.jpg" alt="Third slide">
                 <div class="container">
                     <div class="carousel-caption">
                         <h1>我的错题</h1>
@@ -51,4 +51,135 @@
             <span class="sr-only">Next</span>
         </a>
     </div><!-- /.carousel -->
+
+    
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- two columns of text below the carousel -->
+      <div id="exePro" class="row">
+        <div class="col-lg-6">
+        <!-- <img class="img-circle" src="img/too.jpg" alt="Generic placeholder image" width="140" height="140"> -->
+          
+          <h2><strong>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp真题</strong></h2>
+
+          <hr>
+
+          <ul >
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题请你点击以下....</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题请你点击以下.....</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题请你点击以下.....</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+            <li>
+              <a href="#">一道综合题请你点击以下.....</a> 
+              <span class="fr">2017.3.13</span>
+            </li>
+          </ul>
+          <p><a class="btn btn-default fr" href="#" role="button">阅读更多&raquo;</a></p>
+        </div><!-- /.col-lg-6 -->
+        <div text-decoration:underline class="col-lg-6">
+        <!-- <img class="img-circle" src="img/too.jpg" alt="Generic placeholder image" width="140" height="140"> -->
+          
+           <h2><strong>&nbsp&nbsp&nbsp&nbsp&nbsp易错题</strong></h2>
+
+          <hr class="default">
+
+          <ul>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+            <li><a href="#">这是一道练习题</a><span class="fr">2017.3.13</span></li>
+          </ul>
+          <p><a class="btn btn-default fr" href="#" role="button">阅读更多&raquo;</a></p>
+        </div><!-- /.col-lg-6 -->
+      
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">头脑风暴法 <!-- <span class="text-muted">It'll blow your mind.</span>--></h2>
+          <p class="lead">头脑风暴何以能激发创新思维？根据A·F·奥斯本本人及其他研究者的看法，主要有以下几点：
+                  </br>
+              　　第一，联想反应。联想是产生新观念的基本过程。在集体讨论问题的过程中，每提出一个新的观念，都能引发他人的联想。相继产生一连串的新观念，产生连锁反应，形成新观念堆，为创造性地解决问题提供了更多的可能性。
+                  </br>
+              　　第二，热情感染。在不受任何限制的情况下，集体讨论问题能激发人的热情。人人自由发言、相互影响、相互感染，能形成热潮，突破固有观念的束缚，最大限度地发挥创造性地思维能力</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/too.jpg" alt="jpg" ><!--data-src="holder.js/500x500/auto" alt="Generic placeholder image" -->
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading"> 学习曲线 <!-- <span class="text-muted"></span> --></h2>
+          <p class="lead">学习曲线学习曲线表示了经验与效率之间的关系，指的是越是经常地执行一项任务，每次所需的时间就越少。将学习效果数量化绘制于坐标纸上，横轴代表练习次数（或产量），纵轴代表学习的效果（单位产品所耗时间），这样绘制出的一条曲线，就是学习曲线。学习效果受许多因素的影响，主要有：操作者的动作熟练程度。这是影响学习曲线的最基本因素。管理技术的改善，正确的培训、指导，充分的生产准备与周到的服务，工资奖励及惩罚等管理政策的运用产品设计的改善生产设备与工具的质量各种材料的 连续供应和 质量信息反馈的及时性专业化分工程度</p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="img/too.jpg" alt="img/love-story-03.jpg">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">批判性思考 <!-- <span class="text-muted">Checkmate.</span>--></h2>
+          <p class="lead">我们认为批判性思考是一种有目的而自律的判断，并对判断的基础就证据、概念、方法学、标准釐定、背景因素层面加以诠释、分析、评估、推理与解释……有理想批判性思考能力的人凡事习惯追根究柢，认知务求全面周到，判断必出于理据，心胸保持开放，态度保有弹性，评价必求公正，能坦然面对主观偏见，判断必求谨慎，且必要时愿意重新思量，对争议点清楚了解，处理复杂事物有条不紊，蓃集相关资料勤奋不懈，选取标准务求合理，专注于探索问题，而且在该问题该环境许可的情况下坚持寻求最精确的结果。</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/too.jpg" alt="img/love-story-03.jpg">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
 @stop
