0239c5dae1a4574c7712770e5dbcf1948055549e
Aranl <893916371@qq.com>
2017-03-22 13:19:09 +0800

    调整了.css，.js文件的位置

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,17 +9,18 @@
 <!-- 导航图标 !-->
     <link rel="icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="/css/app.css">
+<!-- 注释掉的.css文件-->
+    <!--<link rel="stylesheet" href="/css/app.css">-->
     <title>@yield('title', 'LLHH')</title>
 
     <!-- Bootstrap core css !-->
-     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/dist/bootstrap.min.css" rel="stylesheet">
        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="css/assets/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
+    <script src="js/assets/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -28,8 +29,8 @@
     <![endif]-->
 
     <!-- Custom styles for this template -->
-    <link href="css/carousel.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+    <link href="css/my/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/my/mystyle.css">
 </head>
     <body>
         @include(('layouts._header'))
@@ -37,17 +38,19 @@
             @include('shared.messages')
             @yield('content')
             @include(('layouts._footer'))
-            
-        <script src="js/app.js"></script>
+
+            <!--被注释掉的.js文件-->
+        <!--<script src="js/app.js"></script>-->
+
             <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/assets/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/dist/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.min.js"></script>
+    <script src="js/assets/vendor/holder.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/assets/ie10-viewport-bug-workaround.js"></script>
     </body>
 </html>
