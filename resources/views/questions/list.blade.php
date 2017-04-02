05ad84e253feac61759776745c2a47867554ce15
lamaaa <554026560@qq.com>
2017-04-02 15:59:49 +0800

    添加分页

--- resources/views/questions/list.blade.php
+++ resources/views/questions/list.blade.php
@@ -3,27 +3,24 @@
 @section('content')
 
 <div class="container">
-  @foreach($modules as $module)
-
-    @endforeach
       <!-- single button -->
       <nav class="bs-docs-sidebar hidden-print hidden-xs hidden-sm affix pull-left hidden:overflow" style="margin-top:100px">
-        <div class="btn-group-vertical" role="group">
+          <div class="btn-group-vertical" role="group">
            @foreach ($modules as $module)
-              @foreach ($module->parts as $part)
-          <div class="btn-group" role="group">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              &nbsp&nbsp&nbsp&nbsp{{ $part->name}}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="caret"></span>
-            </button>
-              <ul class="dropdown-menu" >   
-                  @foreach ($part->chapters as $chapter)
-                    <li><a href="#">{{$chapter->name}}</a></li>
-                  @endforeach
-              </ul>
-          </div>
-              @endforeach
-          @endforeach
-  
+                @foreach ($module->parts as $part)
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-default dropdown-toggle"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        &nbsp&nbsp&nbsp&nbsp{{ $part->name}}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" >
+                        @foreach ($part->chapters as $chapter)
+                            <li><a href="#">{{$chapter->name}}</a></li>
+                        @endforeach
+                        </ul>
+                    </div>
+                @endforeach
+            @endforeach
           </div>
       </nav>
 
@@ -50,9 +47,9 @@
             <option value="3">困难</option>
           </select>
         </div><!--结束方式选项-->
+
       <!--做题面板，头部显示难度系数，入库时间和收集箱按钮；内容框显示题目；尾部显示选项按钮和答案按钮-->
         <div class=" pull-right" style="width:800px">
-   
             @foreach($questions as $question)
               <!--做题面板，头部显示难度系数，入库时间和收集箱按钮；内容框显示题目；尾部显示选项按钮和答案按钮-->
           <div class="panel panel-default" >
@@ -76,43 +73,22 @@
                   <span>收集箱</span>
               </button>
             </div>
+
             <div class="panel-body">
               <p>
                   {!!$question->description!!}
               </p>
             </div>
             <div class="panel-footer">
-              <!--<div class="radio" >-->
-                    <!--<label for="answerA" >&nbsp&nbsp&nbsp&nbsp
-                      <input id="1"  class="#" type="radio" value="option1" onclick="checked" name="answerA" >
-                    A
-                    </label>
-                    <label for="answerB">&nbsp&nbsp&nbsp&nbsp
-                      <input id="2"  class="#" type="radio" value="option2" onclick="checked" name="answerB">
-                      B
-                    </label>
-                    <label for="answerC">&nbsp&nbsp&nbsp&nbsp
-                      <input id="3"  class="#" type="radio" value="option3" onclick="checked" name="answerA" >
-                      C
-                    </label>
-                    <label for="answerD">&nbsp&nbsp&nbsp
-                      <input id="4"  class="#" type="radio" value="option4" onclick="checked" name="answerB">
-                      D
-                    </label>
-                  <span>
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-->
                <button type="button" class="btn-lg  btn-danger"  data-toggle="popover" title="Popover title"
                        data-content="{!!$question->answer!!}">答案
                </button>
-            <!--</div>-->
-          </div>
-        </div><!--结束做题面板-->
-        @endforeach
+            {{--</div>--}}
+            </div>
+          </div><!--结束做题面板-->
+             @endforeach
+                {!! $questions->render() !!}
+        </div>
       </div>
 </div>
 <hr>
